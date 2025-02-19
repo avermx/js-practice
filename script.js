52ebc66f0ac8e45ac8c335aea8c26f1c78e34eda
avermx <aniket125verma@gmail.com>
2025-02-19 18:11:25 +0530

    INIT

--- script.js
+++ script.js
@@ -16,14 +16,99 @@
 
 
 
-let inp = Number(prompt())
-let inp2 = Number(prompt())
+// let inp = Number(prompt())
+// let inp2 = Number(prompt())
 
-let sum = inp + inp2
+// let sum = inp + inp2
 
-if(sum%2 == 0){
-    console.log("even");
+// if(sum % 2 === 0){
+//     console.log("even");
     
-}else{
-    console.log("odd");   
-}
+// }else{
+//     console.log("odd");   
+// }
+
+
+
+
+// let word = prompt()
+
+// if (word == Number(word)){
+//     console.log('number');
+    
+// }
+// else if(word === word.toUpperCase()){
+//     console.log("uppercase");
+    
+// }else if(word === word.toLowerCase()){
+//     console.log('lowercase');
+    
+// }
+
+
+
+
+// let num1 = prompt()
+// let num2 = prompt()
+// let num3 = prompt()
+
+
+// if(num1 > num2 && num1 > num3){
+//     console.log(num1);
+    
+// }
+// else if(num2 > num3 && num2 > num1){
+//     console.log(num2);
+    
+// }
+// else{
+//     console.log(num3);
+    
+// }
+
+
+
+
+// let year = Number(prompt("Enter Year"));
+// if (year % 100 === 0 && year % 400 !== 0 ) {
+//   console.log("Not leap year");
+// } else if (year % 4 === 0) {
+//   console.log("Leap year");
+// } else {
+//   console.log("Not a leap year");
+// }
+
+
+// let num1 = Number(prompt())
+// let opp = prompt("enter opp")
+// let num2 = Number(prompt("enter opp"))
+
+
+
+// if(opp == '+'){
+//     console.log(num1+num2);
+// }
+// else if(opp == '-'){
+//     console.log(num1-num2);
+// }
+// else if(opp == '*'){
+//     console.log(num1*num2);
+// }
+// else if(opp == '/'){
+//     console.log(num1/num2);
+// }
+// else if(opp == '%'){
+//     console.log(num1%num2);
+// }
+
+
+// let num = prompt()
+
+
+// if(num < 0){
+//     console.log("negative");
+    
+// }
+// else if(num > 0){
+//     console.log("pos");
+// }
