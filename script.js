d4bdd529042df11b0486da7716a57d6015deb69e
avermx <aniket125verma@gmail.com>
2025-02-19 15:03:20 +0530

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+// let age = Number(prompt())
+
+
+
+// if(age < 18){
+//     console.log("minor");
+// }
+// else if (age >= 18 && age < 60){
+//     console.log("adult");
+    
+// }else{
+//     console.log("senior");
+    
+// }
+
+
+
+
