4e94771cf531ddad712ea9aa968248d84671f24d
avermx <aniket125verma@gmail.com>
2025-02-20 20:32:31 +0530

    init

--- script.js
+++ script.js
@@ -116,20 +116,200 @@
 
 
 
-let user = prompt("Enter the name")
-let time = Number(prompt("Enter the time"))
+// let user = prompt("Enter the name")
+// let time = Number(prompt("Enter the time"))
 
 
 
-if(time >= 5 && time < 12){
-    console.log("Good Morning" );
-}
-else if (time >= 12 && time < 17){
-    console.log("Good Afternoon" );
-}
-else if(time >= 17 && time < 21){
-    console.log("Good Evening" );
-}
-else {
-    console.log("Good night");
-}
+// if(time >= 5 && time < 12){
+//     console.log("Good Morning" );
+// }
+// else if (time >= 12 && time < 17){
+//     console.log("Good Afternoon" );
+// }
+// else if(time >= 17 && time < 21){
+//     console.log("Good Evening" );
+// }
+// else {
+//     console.log("Good night");
+// }
+
+
+// let user =  prompt("enter color")
+
+
+// if (user === "red"){
+//     console.log("Stop");
+    
+// }
+// else if (user === "yellow"){
+//     console.log("Get Ready");
+    
+// }
+// else if(user === "green"){
+//     console.log("Go");
+    
+// }
+
+
+// for (let i = 1; i <= 10 ; i++) {
+//  console.log(1 * i, 2 * i, 3 * i, 4 * i, 5*i , 6*i, 7*i, 8*i, 9*i,10*i, );
+ 
+    
+// }
+
+
+// let dataname = "aniket";
+
+// let password = 1234
+
+// let username = prompt("enter user name")
+// let userpassword = Number(prompt("enter user Password"))
+
+
+// if (dataname == username && password == userpassword){
+// console.log("Login success");
+// }
+// else{
+//     console.log("wrong username and password");
+    
+// }
+
+
+
+// let a = 10 
+// let b = 5
+
+// console.log(a,b);
+
+// [a,b] = [b,a]
+
+// console.log(a,b);
+
+
+// let a = 15 
+// let b = 10
+// console.log(a,b);
+
+// a = a + b
+
+// b = a - b
+
+// a = a - b
+// console.log(a,b);
+
+
+
+// let user = Number(prompt())
+
+// if(user%3==0 && user%5==0){
+//     console.log("FizzBuzz");
+    
+// }
+// else if (user%3 === 0){
+//     console.log("Fizz");
+    
+// }
+// else if(user%5===0){
+//     console.log("Buzz");
+    
+// }
+// else{
+//     console.log(user);
+    
+// }
+
+
+
+
+
+// let age = prompt('enter your age');
+
+// let z ;
+
+// for (let i = age.length - 1; i >= 0; i--) {
+
+//     z = age[i]
+//     console.log(age[i]);
+// }z
+
+
+// let num = 10
+// let num2 = 10
+
+// console.log(num % num );
+
+
+
+//Q25
+//  let num = Number(prompt())
+
+
+// if(num == 1){
+//     console.log("one");
+    
+// }
+// else if(num == 2){
+//     console.log("Two");
+    
+// }
+// else if(num == 3){
+//     console.log("THree");
+    
+// }
+// else if(num == 4){
+//     console.log("Four");
+    
+// }
+// else if(num == 5){
+//     console.log("Five");
+    
+// }
+
+// Q17
+// let word = prompt()
+
+//     console.log(word);
+    
+//  let rev = word.split('').reverse().join('')
+
+// if(word === rev){
+//     console.log("Palindrome");
+    
+// }else{
+//     console.log("Not a Palindrome");
+// }
+ 
+// let user = prompt("aniket")
+//  let rev = ''
+
+//  for (let i = user.length - 1;i >=0; i--) {
+//     rev += user[i]
+    
+//  }
+//  console.log(rev);
+ 
+
+// let num = prompt("aniket")
+// let num2 = 0
+// for (let i = 0; i < num.length; i++) {
+//     console.log(num[i]);
+    
+//     num2 += Number(num[i])
+    
+    
+// }
+
+// console.log(num2)
+
+
+
+let user = prompt("aniket")
+ let rev = ''
+
+ for (let i = user.length - 1;i >=0; i--) {
+    rev += user[i]
+    
+ }
+ console.log(rev);
+ 
